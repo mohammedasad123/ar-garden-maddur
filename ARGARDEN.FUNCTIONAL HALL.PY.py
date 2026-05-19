f7e72cb4a7c0984ec70254847a4bd391785aaba6
mohammedasad123 <mohammadasad76444@gmail.com>
2026-05-19 14:55:18 +0530

    Add files via upload

---
+++ ARGARDEN.FUNCTIONAL HALL.PY.py
@@ -0,0 +1,17 @@
+# Taj Mahal Function Hall Booking System
+ 
+print("===== AR GARDEN  Function Hall (MADDURs) Booking =====")
+
+name = input("Enter Customer Name: ")
+event = input("Enter Event Type (Wedding/Birthday/Reception): ")
+date = input("Enter Booking Date: ")
+guests = int(input("Enter Number of Guests: "))
+
+print("\n===== Booking Details =====")
+print("Customer Name :", name)
+print("Event Type    :", event)
+print("Booking Date  :", date)
+print("Guests        :", guests)
+
+print("\nBooking Confirmed Successfully!")
+print("Thank you for choosing AR GARDEN  Function Hall (MADDURs).")
